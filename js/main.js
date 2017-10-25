9cf3a6a5e2f5c467ef6c51eef33544a0a8fd99c3
Shuhard89 <shuhard89@gmail.com>
2017-10-25 17:02:15 +0300

    AJAX progress bar added

--- js/main.js
+++ js/main.js
@@ -343,4 +343,19 @@ $(document).ready(function() {
 		basicModal.modalText.html(thisModalTextContent);
 		basicModal.openBasicModal();
 	});
+
+	// jQuery AJAX progress bar
+	$(document).ajaxStart( function () {
+		if ($("#loadingbar").length === 0) {
+			$("body").append("<div id='loadingbar'></div>")
+			$("#loadingbar").addClass("waiting").append($("<dt/><dd/>"));
+			$("#loadingbar").width((50 + Math.random() * 30) + "%");
+		}
+	});
+
+	$(document).ajaxComplete(function () {
+		$("#loadingbar").width("101%").delay(200).fadeOut(400, function () {
+			$(this).remove();
+		});
+	})
 });
