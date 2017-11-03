650b5ec62c3ba0f3d173157305b1fe1048d9acda
Shuhard89 <shuhard89@gmail.com>
2017-11-03 15:37:53 +0200

    Help size charts page updated

--- templates/pages/help/size_charts_template.php
+++ templates/pages/help/size_charts_template.php
@@ -6,74 +6,18 @@
 			<a href="#" class="pa-order-details-back-link help-sidebar-btn">Назад</a>
 		</div>
 		<article>
-			<section class="size-charts-costumes-wrapper">
-				<h2>Костюмы</h2>
-				<p>The measurements given refer to body measurements.</p>
-				<div class="clearfix">
-					<div class="size-charts-costumes-slider">
-						<div class="size-charts-costumes-slider-controls-wrapper clearfix">
-							<span>Размеры</span>
-							<ul class="clearfix">
-								<li>Средний</li>
-								<li>Высокий</li>
-								<li>Низкий</li>
-							</ul>
-						</div>
-						<div class="size-charts-costumes-slider-list-wrapper">
-							<ul>
-								<li>
-									<img src="img/placeholder_content/page_help_size_charts/size_chart_costumes.svg" alt="Costumes Size Chart - Medium">
-								</li>
-								<li>
-									<img src="img/placeholder_content/page_help_size_charts/size_chart_costumes.svg" alt="Costumes Size Chart - Tall">
-								</li>
-								<li>
-									<img src="img/placeholder_content/page_help_size_charts/size_chart_costumes.svg" alt="Costumes Size Chart - Short">
-								</li>
-							</ul>
-						</div>
-					</div>
-					<div class="size-charts-measure-help">
-						<div class="size-charts-measure-help-man-img-wrapper">
-							<div class="size-charts-measure-help-man-img-container">
-								<img src="img/icons/icon_man.svg" alt="Measurement">
-								<span class="size-charts-measure-help-man-circle" id="circle1">1</span>
-								<span class="size-charts-measure-help-man-circle" id="circle2">2</span>
-								<span class="size-charts-measure-help-man-circle" id="circle3">3</span>
-								<span class="size-charts-measure-help-man-circle" id="circle4">4</span>
-								<span class="size-charts-measure-help-man-circle" id="circle5">5</span>
-								<span class="size-charts-measure-help-man-circle" id="circle6">6</span>
-							</div>
-						</div>
-						<div class="size-charts-measure-help-text">
-							<p class="circle1 active">Measure around the middle of your neck, at the Adam's apple.</p>
-							<p class="circle2">Measure around the fullest part of your chest and shoulder blades by dropping your arms loosely to the sides.</p>
-							<p class="circle3">Measure around your natural waistline at the belly button.</p>
-							<p class="circle4">Measure at the widest part of your hip.</p>
-							<p class="circle5">Measure from the middle of back of your neck to the wrist.</p>
-							<p class="circle6">Measure from the top of your inside leg at the crotch to the ground without shoes.</p>
-						</div>
-					</div>
-				</div>
-			</section>
-			<section class="size-charts-shoes-wrapper">
-				<h2>Обувь</h2>
-				<p>The measurements given refer to body measurements.</p>
-				<div class="size-charts-shoes-img-wrapper">
-					<img src="img/placeholder_content/page_help_size_charts/size_chart_shoes.svg" alt="Shoes Size Chart">
-				</div>
-				<div class="size-charts-shoes-care-wrapper">
-					<h3>Golden Rules for Shoe Care</h3>
-					<ol>
-						<li>Wear new shoes for a maximum of two to three hours at a time. </li>
-						<li>Waterproof your shoes after the first day and repeat the process on a regular basis.</li>
-						<li>You will be able to enjoy your shoes for longer when you use a shoe tree after each wear.</li>
-						<li>Do not wear your shoes on two consecutive days; give them a 24 hour rest in between.</li>
-						<li>Treat your shoes regularly with shoe cream (for smooth leather).</li>
-						<li>All leather products, particularly shoes, must be protected from exposure to direct sunlight, moisture and radiator heat.</li>
-					</ol>
-				</div>
-			</section>
+			<div class="t-short-sizes-wrapper">
+				<img src="img/placeholder_content/page_help_size_charts/t_shorts.svg">
+			</div>
+			<div class="costumes-sizes-wrapper">
+				<img src="img/placeholder_content/page_help_size_charts/costumes.svg">
+			</div>
+			<div class="pants-sizes-wrapper">
+				<img src="img/placeholder_content/page_help_size_charts/pants.svg">
+			</div>
+			<div class="shoes-sizes-wrapper">
+				<img src="img/placeholder_content/page_help_size_charts/shoes.svg">
+			</div>
 		</article>
 	</main>
 </div>
