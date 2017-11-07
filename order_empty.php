2e5bc3141735a6b8a3cc75f5d1e958866775aae6
Shuhard89 <shuhard89@gmail.com>
2017-11-07 16:18:25 +0200

    Empty order page added

---
+++ order_empty.php
@@ -0,0 +1,3 @@
+<?php include 'templates/header.php'; ?>
+<?php include 'templates/pages/order_empty_template.php'; ?>
+<?php include 'templates/footer.php'; ?>
---
+++ templates/pages/order_empty_template.php
@@ -0,0 +1,11 @@
+<div class="page-wrapper">
+	<div class="order-page-wrapper">
+		<div class="container-404">
+			<div class="container-404-content">
+				<h1>В вашей корзине пусто</h1>
+				<p>Вы удалили все товары из вашей корзины</p>
+				<a href="index.php" class="go-back-404-btn">Продолжить покупки</a>
+			</div>
+		</div>
+	</div>
+</div>
